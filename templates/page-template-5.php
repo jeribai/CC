ffd96f7ef61ca38aa6fabdd3948faf8d8602d24c
Jeribai Tascoe <jt@newcreationstudio.com>
2013-08-14 10:32:43 -0700

    Fixed template-5

--- templates/page-template-5.php
+++ templates/page-template-5.php
@@ -1,9 +1,17 @@
 <?php include("../inc/header.php") ?>
 
-
-<section id="content" class="page">
-
-<div id="breadcrumb" class="page"><a href="http://columbia.jeribai.com"><i class="icon-home"></i>Home</a> &gt; Page</div>
+<div id="content" class="page">
+	
+	<div id="breadcrumb" class="row">
+		<div class="small-12 columns">
+			<ul class="breadcrumbs">
+			  <li><i class="icon-home"></i><a href="#">Home</a></li>
+			  <li><a href="#">Academics</a></li>
+			  <li><a href="#">Programs</a></li>
+			  <li class="current unavailable"><a href="#">TRiO Student Support Services</a></li>
+			</ul>
+		</div>
+	</div>
 
 	<div class="row">
 		<article class="small-12 columns">
@@ -11,7 +19,7 @@
 			<p>Columbia College provides comprehensive, student  centered academic and vocational education programs, which help prepare career paths and ultimately, increase future opportunities. We offer Associate in Arts Degrees, Associate in Science Degrees, Associate in Science (Occupational Education) Degrees, and Certificates of Achievement that can be completed in two years. The quality of education at our institution can be attributed to dedicated faculty and staff, low student-instructor ratios, and a nurturing learning environment.</p>
 		</article>
 	</div>
-</section><!-- #content -->
+</div><!-- #content -->
 
 
 <?php include("../inc/footer.php") ?>
